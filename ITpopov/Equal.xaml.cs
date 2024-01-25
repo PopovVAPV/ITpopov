0549568a586936010e53a2e60697b6d437e79204
PopovVAPV <popov.va.di11@mail.ru>
2024-01-25 12:05:43 +0300

    Добавьте файлы проекта.

---
+++ ITpopov/Equal.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ITpopov
+{
+    /// <summary>
+    /// Логика взаимодействия для Equal.xaml
+    /// </summary>
+    public partial class Equal : Window
+    {
+        public Equal()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ITpopov/Otchet.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ITpopov
+{
+    /// <summary>
+    /// Логика взаимодействия для Otchet.xaml
+    /// </summary>
+    public partial class Otchet : Window
+    {
+        public Otchet()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
